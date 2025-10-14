9c3751cd25c8c43ef0dca72b469563293cf11275
rmcneil4269 <rmcneil@email.sc.edu>
2025-10-14 13:48:39 -0400

    Fixed data classes, added folders, and everything works :)

--- DataConstants.java
+++
@@ -1,5 +0,0 @@
-public class DataConstants {
-    public final String USERS_FILE = "users.json";
-    public final String ROOMS_FILE = "rooms.json";
-    public final String SAVE_DIRECTORY = "saves/";
-}
---
+++ java/DataConstants.java
@@ -0,0 +1,5 @@
+public class DataConstants {
+    public final String USERS_FILE = "jsons/users.json";
+    public final String ROOMS_FILE = "jsons/rooms.json";
+    public final String SAVE_DIRECTORY = "jsons/saves/";
+}
