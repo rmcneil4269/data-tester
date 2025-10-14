aa9fc57a3c94f0dfcedf4f61584bcb8a4553a895
rmcneil4269 <rmcneil@email.sc.edu>
2025-10-14 13:33:34 -0400

    Create DataConstants.java

---
+++ DataConstants.java
@@ -0,0 +1,5 @@
+public class DataConstants {
+    public final String USERS_FILE = "users.json";
+    public final String ROOMS_FILE = "rooms.json";
+    public final String SAVE_DIRECTORY = "saves/";
+}
